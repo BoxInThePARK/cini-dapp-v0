8d0017a1e5fca885337ebda8a274df51c712a7ea
Dan Hsu <hsu86110@gmail.com>
2022-07-28 20:43:57 +0800

    feature: modified ExampleApp to CaptureToEarnApp

--- android/app/src/main/java/com/exampleapp/newarchitecture/MainApplicationReactNativeHost.java
+++ android/app/src/main/java/com/exampleapp/newarchitecture/MainApplicationReactNativeHost.java
@@ -1,4 +1,4 @@
-package com.exampleapp.newarchitecture;
+package com.capturetoearnapp.newarchitecture;
 
 import android.app.Application;
 import androidx.annotation.NonNull;
@@ -19,9 +19,9 @@ import com.facebook.react.fabric.CoreComponentsRegistry;
 import com.facebook.react.fabric.FabricJSIModuleProvider;
 import com.facebook.react.fabric.ReactNativeConfig;
 import com.facebook.react.uimanager.ViewManagerRegistry;
-import com.exampleapp.BuildConfig;
-import com.exampleapp.newarchitecture.components.MainComponentsRegistry;
-import com.exampleapp.newarchitecture.modules.MainApplicationTurboModuleManagerDelegate;
+import com.capturetoearnapp.BuildConfig;
+import com.capturetoearnapp.newarchitecture.components.MainComponentsRegistry;
+import com.capturetoearnapp.newarchitecture.modules.MainApplicationTurboModuleManagerDelegate;
 import java.util.ArrayList;
 import java.util.List;
 
