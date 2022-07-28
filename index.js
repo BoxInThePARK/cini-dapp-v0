1c53d562b7e0d3121da3001f4f801085b2e909d6
Dan Hsu <hsu86110@gmail.com>
2022-07-28 20:24:33 +0800

    feature: modified file structure

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ import 'react-native-get-random-values';
 import 'react-native-url-polyfill/auto';
 
 import {AppRegistry} from 'react-native';
-import App from './App';
+import App from './src/App';
 import {name as appName} from './app.json';
 
 // Mock event listener functions to prevent them from fataling.
