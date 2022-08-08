2409c998a445c1177d5c3e476ec53046ac282978
Dan Hsu <hsu86110@gmail.com>
2022-08-09 01:02:07 +0800

    feature: add Tab component on UserProfile Page

--- src/screens/UserProfileScreen.tsx
+++ src/screens/UserProfileScreen.tsx
@@ -4,12 +4,17 @@ import type {Routes} from './Routes';
 import {View, Text, StyleSheet, Image} from 'react-native';
 import {Button} from 'react-native-paper';
 import RNFS from 'react-native-fs';
+import TabBar from '../components/TabBar';
+import {SAFE_AREA_PADDING} from '../utils/constants';
+
+const TAB_LIST = ['developed', 'for sale', 'collected', 'undeveloped'];
 
 type Props = NativeStackScreenProps<Routes, 'UserProfilePage'>;
 
 const UserProfileScreen = ({navigation}: Props) => {
   const [hasMediaLoaded, setHasMediaLoaded] = useState(false);
   const [imageList, setImageList] = useState<String[]>(['']);
+  const [selectedTab, setSelectedTab] = useState(0);
 
   const getImageList = useCallback(async () => {
     try {
@@ -22,7 +27,6 @@ const UserProfileScreen = ({navigation}: Props) => {
         .map(item => item.path);
 
       setImageList(imageList);
-
     } catch (err) {
       console.log(err);
     }
@@ -35,17 +39,37 @@ const UserProfileScreen = ({navigation}: Props) => {
 
   return (
     <View style={styles.container}>
-      <Text style={styles.buttonText}>UserProfileScreen</Text>
-      {imageList.map((source, index) => (
+      <View style={styles.header}>
         <Image
-          key={index}
-          source={{uri: `file://${source}`}}
-          style={styles.img}
+          source={require('../assets/img/template_profile_cover.png')}
+          style={styles.coverImg}
           resizeMode="cover"
-          // onLoadEnd={onMediaLoadEnd}
-          // onLoad={onMediaLoad}
         />
-      ))}
+        <Image
+          source={require('../assets/img/pfp.png')}
+          style={styles.pfpImg}
+          resizeMode="cover"
+        />
+        <View style={styles.userNameBox}>
+          <Text style={styles.userNameText}>@nearop</Text>
+        </View>
+      </View>
+
+      <View style={styles.ContentWrapper}>
+        <TabBar tabList={TAB_LIST} currentTabIndex={selectedTab} setPressedTab={setSelectedTab} />
+
+        {imageList.map((source, index) => (
+          <Image
+            key={index}
+            source={{uri: `file://${source}`}}
+            style={styles.img}
+            resizeMode="cover"
+            // onLoadEnd={onMediaLoadEnd}
+            // onLoad={onMediaLoad}
+          />
+        ))}
+      </View>
+
       {/* <Button
         style={{width: '80%', height: 52, backgroundColor: '#279AF1'}}
         contentStyle={styles.startButton}
@@ -63,8 +87,49 @@ const styles = StyleSheet.create({
     width: '100%',
     height: '100%',
     backgroundColor: '#F1E8DF',
-    flexDirection: 'row',
-    flexWrap: 'wrap',
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+  },
+  header: {
+    width: '100%',
+    position: 'relative',
+  },
+  coverImg: {
+    width: '100%',
+    height: 180,
+    // zIndex: 1,
+    marginBottom: 80,
+  },
+  pfpImg: {
+    position: 'absolute',
+    width: 72,
+    height: 72,
+    borderRadius: 36,
+    top: 142,
+    left: 60,
+    zIndex: 10,
+  },
+  userNameBox: {
+    position: 'absolute',
+    width: 90,
+    height: 32,
+    backgroundColor: '#ffffff',
+    borderRadius: 16,
+    top: 164,
+    left: 120,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  userNameText: {
+    fontSize: 14,
+    fontWeight: '500',
+    lineHeight: 32,
+    color: '#262626',
+  },
+  ContentWrapper: {
+    width: '100%',
+    height: '100%',
+    paddingHorizontal: SAFE_AREA_PADDING.paddingLeft,
   },
   startButton: {
     width: '100%',
