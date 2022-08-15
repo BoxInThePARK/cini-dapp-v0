b66392ce25a104aea2ce8398cc0c8681c09a31b4
Dan Hsu <hsu86110@gmail.com>
2022-08-16 02:03:55 +0800

    feature: 1. add wallet page 2. modified navbar

---
+++ src/screens/WalletScreen.tsx
@@ -0,0 +1,21 @@
+import React, {useCallback, useContext, useEffect, useState} from 'react';
+import {
+  Dimensions,
+  Image,
+  PermissionsAndroid,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from 'react-native';
+
+const WalletScreen = () => {
+  return (
+    <View>
+      <Text>Wallet Screen</Text>
+    </View>
+  );
+};
+
+export default WalletScreen;
