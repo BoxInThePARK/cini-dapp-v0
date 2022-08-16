f1ba788f3540659c7048a49f7c6af16d506b3218
Dan Hsu <hsu86110@gmail.com>
2022-08-16 19:30:15 +0800

    feature: add image on Single Photo page

--- src/screens/SinglePhotoScreen.tsx
+++ src/screens/SinglePhotoScreen.tsx
@@ -15,16 +15,43 @@ import {
   View,
 } from 'react-native';
 
+const windowWidth = Dimensions.get('window').width;
+
 type Props = NativeStackScreenProps<Routes, 'SinglePhoto'>;
 
-const SinglePhotoScreen = ({navigation}: Props) => {
+const SinglePhotoScreen = ({navigation, route}: Props) => {
+  const {imageSource, imageRatio, creatorInfo} = route.params;
+
+  useEffect(() => {
+    console.log('imageSource', imageSource);
+    console.log('imageRatio', imageRatio);
+    console.log('creatorInfo', creatorInfo);
+  }, []);
+
   return (
-    <View>
+    <View style={styles.screenContainer}>
+      <Image
+        source={{uri: `file://${imageSource}`}}
+        style={{
+          width: windowWidth,
+          aspectRatio: imageRatio,
+        }}
+        resizeMode="cover"
+      />
       <Text>SinglePhoto Screen</Text>
     </View>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  screenContainer: {
+    width: '100%',
+    height: '100%',
+    backgroundColor: '#FFFFFF',
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+  },
+  PhotoWrapper: {},
+});
 
 export default SinglePhotoScreen;
