46ce72cfc00315362bfc1b392ce3444be6f64c17
Dan Hsu <hsu86110@gmail.com>
2022-08-16 22:33:17 +0800

    feature: 1. modified data structure of ROLL_FILM 2. modified layout of Home page

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,7 +1,11 @@
-import {Dimensions, Platform} from 'react-native';
+import {Dimensions, Platform, ImageSourcePropType} from 'react-native';
 import StaticSafeAreaInsets from 'react-native-static-safe-area-insets';
 
 export const CONTENT_SPACING = 15;
+const BERLIN_ROLL_NFT = require('../assets/img/3d-film-roll/berlin.png');
+const LISBON_ROLL_NFT = require('../assets/img/3d-film-roll/lisbon.png');
+const SEOUL_ROLL_NFT = require('../assets/img/3d-film-roll/seoul.png');
+const TAIPEI_ROLL_NFT = require('../assets/img/3d-film-roll/taipei.png');
 
 const SAFE_BOTTOM =
   Platform.select({
@@ -28,19 +32,27 @@ export const SCREEN_HEIGHT = Platform.select<number>({
 // Capture Button
 export const CAPTURE_BUTTON_SIZE = 78;
 
+export const ROLL_FILM_SRC: Record<string, ImageSourcePropType> = {
+  berlin: BERLIN_ROLL_NFT,
+  lisbon: LISBON_ROLL_NFT,
+  seoul: SEOUL_ROLL_NFT,
+  taipei: TAIPEI_ROLL_NFT,
+};
+
 export const ROLL_FILM: Record<string, Record<string, string>> = {
   BERLIN: {
     key: 'berlin',
     display: 'BERLIN 400',
     shots: '36',
-    src: require('../assets/img/3d-film-roll/berlin.png'),
+    // src: BERLIN_ROLL_NFT,
     backgroundColor: '#000',
+    color: '#fff',
   },
   LISBON: {
     key: 'lisbon',
     display: 'LISBON 400',
     shots: '36',
-    src: require('../assets/img/3d-film-roll/lisbon.png'),
+    // src: '../assets/img/3d-film-roll/lisbon.png',
     backgroundColor: '#fff',
     color: '#000',
   },
@@ -48,19 +60,21 @@ export const ROLL_FILM: Record<string, Record<string, string>> = {
     key: 'seoul',
     display: 'SEOUL 400',
     shots: '36',
-    src: require('../assets/img/3d-film-roll/seoul.png'),
+    // src: '../assets/img/3d-film-roll/seoul.png',
     backgroundColor: '#08004E',
+    color: '#fff',
   },
   TAIPEI: {
     key: 'taipei',
     display: 'TAIPEI 800',
     shots: '24',
-    src: require('../assets/img/3d-film-roll/taipei.png'),
+    // src: '../assets/img/3d-film-roll/taipei.png',
     backgroundColor: '#BDEBA0',
+    color: '#000',
   },
 };
 
-export const MockRollFilm: Record<string, Record<string, string, number>> = {
+export const MockRollFilm: Record<string, Record<string, string>> = {
   CINI: {
     key: 'cini',
     display: 'CINI 100',
