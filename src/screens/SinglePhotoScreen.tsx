6402ac0f91e811d0b9e3b3ef55a103c26cd0cda6
Dan Hsu <hsu86110@gmail.com>
2022-08-16 21:05:25 +0800

    feature: titleBox on SinglePhoto done

--- src/screens/SinglePhotoScreen.tsx
+++ src/screens/SinglePhotoScreen.tsx
@@ -16,14 +16,61 @@ import {
 } from 'react-native';
 import IonIcon from 'react-native-vector-icons/Ionicons';
 import {MockRollFilm} from '../utils/constants';
+import {SAFE_AREA_PADDING} from '../utils/constants';
 
 const windowWidth = Dimensions.get('window').width;
 
 type Props = NativeStackScreenProps<Routes, 'SinglePhoto'>;
 
+interface PhotoContentProps {
+  navigation: NativeStackNavigationProp<Routes, 'SinglePhoto', undefined>;
+}
+
+const PhotoContent = ({navigation}: PhotoContentProps) => {
+  return (
+    <ScrollView
+      style={{
+        width: '100%',
+        flexGrow: 1,
+      }}>
+      <View
+        style={{
+          width: '100%',
+          height: '100%',
+          paddingHorizontal: SAFE_AREA_PADDING.paddingLeft + 6,
+          justifyContent: 'flex-start',
+        }}>
+        <View style={styles.titleBox}>
+          <View style={styles.photoNameBox}>
+            <Text style={styles.photoNameText}>JUST A BUSAN BOY</Text>
+            <Text style={styles.photoDateText}>Aug 7, 2022</Text>
+          </View>
+          <View style={styles.creatorWrapper}>
+            <Text style={styles.creatorTitleText}>Created by</Text>
+            <View style={styles.userInfoBox}>
+              <TouchableOpacity
+                style={styles.userInfoContent}
+                onPress={() => {
+                  navigation.navigate('UserProfilePage', {initialTab: 0});
+                }}>
+                <Image
+                  source={require('../assets/img/pfp.png')}
+                  style={styles.userPfp}
+                  resizeMode="cover"
+                />
+                <Text style={styles.userNameText}>@nearop</Text>
+              </TouchableOpacity>
+            </View>
+          </View>
+        </View>
+      </View>
+    </ScrollView>
+  );
+};
+
 const SinglePhotoScreen = ({navigation, route}: Props) => {
   const {imageSource, imageRatio, creatorInfo} = route.params;
-  const [filmRollName, setFilmRollName] = useState('Loading...');
+  const [filmRollName, setFilmRollName] = useState<string>('Loading...');
 
   useEffect(() => {
     if (imageSource) {
@@ -64,8 +111,7 @@ const SinglePhotoScreen = ({navigation, route}: Props) => {
           </TouchableOpacity>
         </View>
       )}
-
-      <Text>SinglePhoto Screen</Text>
+      <PhotoContent navigation={navigation} />
     </View>
   );
 };
@@ -113,11 +159,50 @@ const styles = StyleSheet.create({
     alignItems: 'center',
   },
   filmRollText: {
-    fontFamily: 'Montserrat-Bold',
+    fontFamily: 'Montserrat-SemiBold',
     fontSize: 12,
     lineHeight: 20,
     color: '#262626',
     marginLeft: 4,
+  },
+  titleBox: {
+    width: '100%',
+    height: 80,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    paddingBottom: 4,
+  },
+  test: {},
+
+  photoNameBox: {
+    width: 200,
+    height: '100%',
+    justifyContent: 'flex-end',
+  },
+  photoNameText: {
+    fontFamily: 'Montserrat-Bold',
+    fontSize: 18,
+    lineHeight: 20,
+    color: '#000000',
+  },
+  photoDateText: {
+    fontFamily: 'Montserrat-SemiBold',
+    fontSize: 12,
+    lineHeight: 18,
+    color: '#9E9E9E',
+  },
+  creatorWrapper: {
+    width: 84,
+    height: '100%',
+    justifyContent: 'flex-end',
+    position: 'relative',
+  },
+  creatorTitleText: {
+    fontFamily: 'Montserrat-SemiBold',
+    fontSize: 8,
+    lineHeight: 15,
+    color: '#9E9E9E',
   },
   userInfoBox: {
     width: 72,
@@ -127,14 +212,12 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     borderRadius: 10,
     backgroundColor: '#FFFFFF',
-    position: 'absolute',
-    top: 8,
-    left: 16,
     shadowOffset: {width: 0, height: 2},
     shadowRadius: 5,
     shadowColor: '#000000',
     shadowOpacity: 0.4,
-    elevation: 20,
+    elevation: 5,
+    marginBottom: 4,
   },
   userInfoContent: {
     width: '100%',
